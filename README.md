75523632914e483e0156efdd94a3fa65e14a2edc
giacomo9999 <gmarchesi888@gmail.com>
2021-01-02 18:11:33 -0500

    Adopted a more declarative approach using JSX.

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ Note: node-sass seems to be deprecated. The earlier 4.14.1 version needs to be u
 
 If necssary to remove 5.0: npm uninstall node-sass
 Then: npm install node-sass@4.14.1
+
+Useful:
+https://wattenberger.com/blog/react-and-d3
+https://blog.logrocket.com/data-visualization-in-react-using-react-d3-c35835af16d0
--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from "react";
-import RowChart from "./RowChart";
+// import RowChart from "./RowChart";
+import TestThing from "./TestThing";
 import "./App.scss";
+import * as d3 from "d3";
 
 class App extends Component {
+  componentDidMount() {
+    d3.select("rect:nth-child(2").style("fill", "white");
+  }
+
   render() {
     return (
       <div className="mainPage">
-        MAIN PAGE
-        <RowChart />
+        <h1>MAIN PAGE</h1>
+        <TestThing height={500} width={700} />
       </div>
     );
   }
