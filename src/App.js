ad7708caeaff74823eb8dcca1bd603b40779f77c
giacomo9999 <gmarchesi888@gmail.com>
2021-01-03 15:15:48 -0500

    Moved TestThing property values to state of App.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React, { Component } from "react";
 // import RowChart from "./RowChart";
 import TestThing from "./TestThing";
-// import Circle from "./Circle";
 import "./App.scss";
 import * as d3 from "d3";
 
 class App extends Component {
+  state = { testThingHeight: 600, testThingWidth: 600, numberOfCircles: 6 };
+  
   componentDidMount() {
     d3.select("circle").attr("fill", "deeppink");
   }
 
   changeStuff() {
-    d3.select("#thaNotoriousSVG").style("background", "blue");
+    const svg = d3.select("#thaNotoriousSVG");
 
-    d3.select("svg")
+    svg.style("background", "blue");
+
+    svg
       .append("circle")
       .attr("r", 20)
       .attr("cx", 300)
@@ -25,7 +28,11 @@ class App extends Component {
     return (
       <div className="mainPage">
         <h1>MAIN PAGE</h1>
-        <TestThing height={600} width={600} numOfCircles={6} />
+        <TestThing
+          height={this.state.testThingHeight}
+          width={this.state.testThingWidth}
+          numOfCircles={this.state.numberOfCircles}
+        />
         <button onClick={this.changeStuff}>CHANGE STUFF</button>
       </div>
     );
